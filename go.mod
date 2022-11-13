cea453e691b60fa863caf8d2a58a387e7ee01cf1
Popov Pavel <pashapdev@yandex.ru>
2022-11-13 05:58:05 +0300

    add app (#2)

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/pashapdev/observer_server
 
 go 1.19
+
+require github.com/go-chi/chi v1.5.4
---
+++ internal/application/app.go
@@ -0,0 +1,74 @@
+package application
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/pashapdev/observer_server/internal/config"
+	"github.com/pashapdev/observer_server/internal/router"
+)
+
+type App struct {
+	handler http.Handler
+	conf    *config.Config
+	server  *http.Server
+}
+
+type AppOption func(*App)
+
+func New(conf *config.Config, opts ...AppOption) *App {
+	app := new(App)
+	app.conf = conf
+	app.handler = router.New()
+	app.server = &http.Server{
+		Addr:        conf.Address,
+		Handler:     router.New(),
+		ReadTimeout: 5 * time.Minute,
+	}
+	return app
+}
+
+func (a *App) Run() error {
+	log.Println("starting http server...")
+	err := a.server.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		log.Println("server was stop with err", err)
+		return err
+	}
+	log.Println("server was stop")
+	return nil
+}
+
+func (a *App) stop(ctx context.Context) error {
+	log.Println("shutdowning server...")
+	err := a.server.Shutdown(ctx)
+	if err != nil {
+		log.Println("server was shutdown with error", err)
+		return err
+	}
+	log.Println("server was shutdown")
+	return nil
+}
+
+func (a *App) GracefulStop(serverCtx context.Context, sig <-chan os.Signal, serverStopCtx context.CancelFunc) {
+	<-sig
+	var timeOut = 30 * time.Second
+	shutdownCtx, shutdownStopCtx := context.WithTimeout(serverCtx, timeOut)
+
+	go func() {
+		<-shutdownCtx.Done()
+		if shutdownCtx.Err() == context.DeadlineExceeded {
+			log.Fatal("graceful shutdown timed out.. forcing exit.")
+		}
+	}()
+
+	err := a.stop(shutdownCtx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	serverStopCtx()
+	shutdownStopCtx()
+}
